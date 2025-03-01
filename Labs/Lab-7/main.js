6d0aaa27a726a84b5fc89f7a438341d69b9b9bf2
shafikcmt <mdshafiqulislam822@gmail.com>
2025-03-01 10:57:57 +0530

    Last updated 1-03-2025

---
+++ Labs/Lab-7/main.js
@@ -0,0 +1,8 @@
+function msg(){
+    window.alert('Hello Students, Good Morning!!');
+}
+
+function changeBG(){
+    var input = document.getElementById('input');
+    input.style.backgroundColor = 'yellow';
+}
